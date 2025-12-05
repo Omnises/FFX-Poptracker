a98fee9618ae909e9502d216bf1407e54cff88f4
Andrewki44 <andrewkilbourn1996@gmail.com>
2025-12-05 17:03:39 +1100

    Liki Logic

--- scripts/logic/party_members.lua
+++ scripts/logic/party_members.lua
@@ -8,7 +8,7 @@ function hasKimahri()
 end
 
 function hasLulu()
-    return has("partymember:Lulu")
+    return has("partymember:lulu")
 end
 
 function hasRikku()
@@ -80,6 +80,73 @@ function hasAnyAeon()
         hasAnima() or hasBahamut() or hasIfrit() or hasIfrit or
         hasMagusSisters() or hasShiva() or hasValefor() or hasYojimbo
     ) then
+        return true
+    else
+        return false
+    end
+end
+
+function hasSwimmers(quantity)
+    local quantity = tonumber(quantity)
+    local counter = 0
+
+    if hasTidus() then
+        counter = counter + 1
+    end
+
+    if hasWakka() then
+        counter = counter + 1
+    end
+
+    if hasRikku() then
+        counter = counter + 1
+    end
+
+    if counter >= quantity then
+        return true
+    else
+        return false
+    end
+end
+
+function hasPartyMembers(quantity)
+    local quantity = tonumber(quantity)
+    local counter = 0
+
+    if hasTidus() then
+        counter = counter + 1
+    end
+
+    if hasWakka() then
+        counter = counter + 1
+    end
+
+    if hasLulu() then
+        counter = counter + 1
+    end
+
+    if hasYuna() then
+        counter = counter + 1
+    end
+
+    if hasKimahri() then
+        counter = counter + 1
+    end
+
+    if hasAuron() then
+        counter = counter + 1
+    end
+
+    if hasRikku() then
+        counter = counter + 1
+    end
+
+    if hasSeymour() then
+        counter = counter + 1
+    end
+
+    print("counter: " .. counter)
+    if counter >= quantity then
         return true
     else
         return false
